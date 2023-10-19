5d2bbbc6b4029279e6560aca8a5cee8487370bd2
nicoalessandro <nico.ingenieria.utn@gmail.com>
2023-10-19 10:36:38 -0300

    PROYECTO FUNCIONAL adesion de emitir_reporte(), mostrar() y liberar(), se comenta mostrar_reservas()

--- main.cpp
+++ main.cpp
@@ -29,10 +29,12 @@ void mostrarMenu();
 void cargar_archivo(const string &nombre_archivo);
 void procesar_archivo(const string &nombre_archivo, Nodo* tabla[num_ciudades][num_dias]);
 void insertar_lista(Nodo* &p, int DNI);
-void emitir_reporte();
+void emitir_reporte(Nodo* tabla[num_ciudades][num_dias]);
+void mostrar(Nodo* p, int &c);
+void liberar(Nodo*& p);
 
 //funciones de control:
-void mostrar_reservas(const string &nombre_archivo);
+/*void mostrar_reservas(const string &nombre_archivo);*/
 
 
 //_____________________________________________________COMIENZO DE LA FUNCION MAIN___________________________________________
@@ -65,14 +67,20 @@ int main() {//-------------------->MAIN(): INT C, INT OPCION;
 
 	
 	do {
-		system("cls");
-		cout << ------------------------------------------------------------------------------------------------"<<endl;
+		cout << "------------------------------------------------------------------------------------------------"<<endl;
 		cout << "PRESIONE ENTER PARA CONTINUAR!!!!"<<endl;
-		cout << ------------------------------------------------------------------------------------------------"<<endl;
+		cout << "------------------------------------------------------------------------------------------------"<<endl;
 		cin.get();
+		system("cls");
         mostrarMenu(); // Llamamos a la función de menú para mostrar las opciones
-        cout << "Selecciona una opción: ";
+        cout << endl;
+		cout << "Selecciona una opcion: ";
         cin >> opcion;
+        cout << "------------------------------------------------------------------------------------------------"<<endl;
+		cout << "PRESIONE ENTER PARA CONTINUAR!!!!"<<endl;
+		cout << "------------------------------------------------------------------------------------------------"<<endl;
+		cin.get();
+		system("cls");
 
         switch (opcion){
             
@@ -82,11 +90,11 @@ int main() {//-------------------->MAIN(): INT C, INT OPCION;
                 break;
 			case 2:
                 procesar_archivo(nombre_archivo, tabla);
-                mostrar_reservas(nombre_archivo);
+                /*mostrar_reservas(nombre_archivo);*/
                 c=0;
 				break;
 		    case 3:
-                emitir_reporte();
+                emitir_reporte(tabla);
                 c=0;
                 break;
 			case 4:
@@ -95,20 +103,20 @@ int main() {//-------------------->MAIN(): INT C, INT OPCION;
 			default://-------------------------->En caso de valores invalidos
             c++; //----------------------------->Cuenta errores consecutivos
                 if (c == 3){//------------------>Si tenes 3 errores sale del programa
-                	out << ------------------------------------------------------------------------------------------------"<<endl;
+                	cout << "------------------------------------------------------------------------------------------------"<<endl;
                 	cout << "Ya ha intentado 3 veces consecutivas una opcion incorrecta... " << endl;
                 	cout << "¡¡¡Ud debe aprender a leer consignas!!!" << endl;
                 	cout << "¡¡¡DE TODOS LOS USUARIOS QUE TUVE USTED FUE EL PEOR!!!" << endl;
 					cout << "PRESIONE ENTER PARA CONTINUAR!!!!"<<endl;
-					cout << ------------------------------------------------------------------------------------------------"<<endl;
+					cout << "------------------------------------------------------------------------------------------------"<<endl;
 					cin.get();
                 	cout << "Saliendo del programa." << endl;
                 	opcion = 4;
 				}
             	else {//------------------------------------>Si falla sin llegar a 3
-            	cout << ------------------------------------------------------------------------------------------------"<<endl;
-                cout << "Opción no válida. Inténtalo de nuevo." << endl;
-                cout << ------------------------------------------------------------------------------------------------"<<endl;
+            	cout << "------------------------------------------------------------------------------------------------"<<endl;
+                cout << "Opción no valida. Intentalo de nuevo." << endl;
+                cout << "------------------------------------------------------------------------------------------------"<<endl;
                 }
 				break;
 		}
@@ -122,7 +130,7 @@ int main() {//-------------------->MAIN(): INT C, INT OPCION;
 
 void mostrarMenu() { //------------------------------------------------------->FUNCION MOSTRAR MENU()
     
-    cout << "Menú:" << std::endl;
+    cout << "__MENU:__" << std::endl;
     cout << "1. Cargar archivo" << endl;
     cout << "2. Procesar archivo" << endl;
     cout << "3. Emitir reporte" << endl;
@@ -151,7 +159,7 @@ void cargar_archivo(const string &nombre_archivo){ //------------------------->C
 
     do{//-----------------------------------------> PRIMER VALOR O SALIR DEL REGISTRO
     	v=1;
-		cout << "Ingrese el código de la ciudad (0..99) o -1 para terminar: ";
+		cout << "Ingrese el codigo de la ciudad (0..99) o -1 para terminar: ";
     	cin >> reserva.cod_ciudad;
     	if(reserva.cod_ciudad<-1 || reserva.cod_ciudad>99){
     		cout << "ERROR!!! HA INGRESADO UN VALOR INCORRECTO!!! "<< endl;
@@ -164,7 +172,7 @@ void cargar_archivo(const string &nombre_archivo){ //------------------------->C
         
         do{//----------------------->INGRESAR DIA HASTA QUE SEA VALIDO
     		v=1;
-			cout << "Ingrese el código del día (0..6): ";
+			cout << "Ingrese el codigo del dia (0..6): ";
     		cin >> reserva.cod_dia;
     		if(reserva.cod_dia<0 || reserva.cod_dia>6){
     			cout << "ERROR!!! HA INGRESADO UN VALOR INCORRECTO!!! "<< endl;
@@ -190,7 +198,7 @@ void cargar_archivo(const string &nombre_archivo){ //------------------------->C
         
 	    do{//-----------------------------------> SIGUIENTE VALOR O SALIR DEL REGISTRO
     		v=1;
-			cout << "Ingrese el código de la ciudad (0..99) o -1 para terminar: ";
+			cout << "Ingrese el codigo de la ciudad (0..99) o -1 para terminar: ";
     		cin >> reserva.cod_ciudad;
     		if(reserva.cod_ciudad<-1 || reserva.cod_ciudad>99){
     			cout << "ERROR!!! HA INGRESADO UN VALOR INCORRECTO!!! "<< endl;
@@ -209,7 +217,7 @@ void procesar_archivo(const string &nombre_archivo, Nodo* tabla[num_ciudades][nu
 	
 	/*La función procesar_archivo: que tiene dos parámetros, de entrada el archivo que se
 	generó en el punto anterior, y de salida la matriz donde se cargarán los punteros.*/
-	
+	int c=0;
 	cout << "Procesando Archivo..."<< endl;
 	Reserva reserva;//-------------------->Declaracion de variable local en el ambito de reserva
     ifstream file(nombre_archivo.c_str(), ios::in | ios::binary);//--------->Apertura de archivo
@@ -218,9 +226,11 @@ void procesar_archivo(const string &nombre_archivo, Nodo* tabla[num_ciudades][nu
         return;
     }
     while (file.read(reinterpret_cast<char*>(&reserva), sizeof(Reserva))) {
-        cout << "Ciudad: " << reserva.cod_ciudad << ", Dia: " << reserva.cod_dia << ", DNI: " << reserva.DNI << endl;
+    	c++;
+        cout <<"Procesando Registro N"<<c<< "... Ciudad: " << reserva.cod_ciudad << ", Dia: " << reserva.cod_dia << ", DNI: " << reserva.DNI << endl;
         insertar_lista(tabla[reserva.cod_ciudad][reserva.cod_dia],reserva.DNI);//----------->tabla[i,j] es puntero y reserva.dni es dato nuevo.
     }
+    cout <<"SE A FINALIZADO EL PROCESAMIENTO DE REGISTROS CON EXITO !"<<endl;
 }//-------------------------------->Fin de PROCESAR_ARCHIVO()
 
 //____________________________________________________________________________________________________________________________________
@@ -242,15 +252,50 @@ void insertar_lista(Nodo* &p, int DNI){        //-------------------------------
 	
 //____________________________________________________________________________________________________________________________________
 
-void emitir_reporte(){
+void emitir_reporte(Nodo* tabla[num_ciudades][num_dias]){
 	/*La función emitir_reporte: que recibe la matriz creada en el punto anterior e imprime
 	por pantalla un listado ordenado por cod_ciudad ascendente, con la cantidad total de
 	reservas efectuadas para cada ciudad.*/
+	int i, j, b=0, cuenta=0;
 	cout << "Emitiendo Reporte..."<< endl;
-	
+	for(i=0;i<num_ciudades;i++){
+		cuenta = 0;
+		for(j=0;j<num_dias;j++){
+			if (tabla [i][j]!=NULL){
+				if (b!=1){
+					cout << "Para la Ciudad N" << i<<" :"<< endl;//--------->Solo se presentara un resultado si hay registros
+					b=1;//-------------------------------------------------->Sistema de bandera para que no se repita el dia.
+				}
+				cout << "Se anotaron el dia N" << j <<" :"<< endl;
+				mostrar(tabla[i][j], cuenta);
+				liberar(tabla[i][j]);
+			}
+		}
+		if (b==1){
+			cout << "Para la Ciudad N " << i<<" la cantidad de Reservas es de: "<< cuenta << endl;
+		}
+		b=0;
 	}
+}
 
-//____________________________________________________________________________________________________________________________________
+void mostrar(Nodo* p, int &c) {
+	Nodo* aux = p;
+	while( aux!=NULL ) {
+		cout << aux->dni << endl;
+		aux = aux->sig;
+		c= c+1;
+	}
+}
+
+void liberar(Nodo*& p) {
+	Nodo* aux;
+	while ( p!=NULL ){
+		aux = p; p = p->sig; delete aux;
+		}
+}
+
+
+/*/____________________________________________________________________________________________________________________________________
 
 void mostrar_reservas(const string &nombre_archivo) {//---------------------------------->MOSTRAR RESERVAS(&NOMBRE_ARCHIVOS): RESERVA
 	// Declaracion de variable local en el ambito de reserva
@@ -273,5 +318,5 @@ void mostrar_reservas(const string &nombre_archivo) {//-------------------------
 }
 
 //_____________________________________________________________________________________________________________________________________
-
+*/
 
